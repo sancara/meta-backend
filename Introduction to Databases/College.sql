cb11467dcc98d653c62fec61bf3faecbdd3b0549
Sancara <santiagocaraballo@gmail.com>
2022-08-30 23:03:26 -0300

    College.sql: minimum sql syntax comprenhension

---
+++ Introduction to Databases/College.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE college;
+
+CREATE TABLE Student;
+
+INSERT INTO Student (column1, column2) VALUES(value1, value2);
+UPDATE Student SET column_name = value_for_column WHERE condition; 
+
+DELETE FROM Student WHERE condition;
+
+SELECT column1, column2 FROM Student WHERE condition;
+
+-- ERD Entity relationship diagram -> graphic representation of the db
+-- Cardinalidad type of relationship, one to one, one to many, many to many
+
